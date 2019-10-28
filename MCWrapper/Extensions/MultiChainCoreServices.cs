bed8de68a0795612f9d6749ef22b4a23e8da7d50
scanlifter <ryan@ryangoodwin.dev>
2019-10-27 22:26:37 -0700

    Add project files.

---
+++ MCWrapper/Extensions/MultiChainCoreServices.cs
@@ -0,0 +1,72 @@
+﻿using MCWrapper.CLI.Extensions;
+using MCWrapper.CLI.Options;
+using MCWrapper.Factory;
+using MCWrapper.Ledger.Entities.Options;
+using MCWrapper.RPC.Extensions;
+using MCWrapper.RPC.Options;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Runtime.InteropServices;
+
+namespace MCWrapper.Extensions
+{
+    /// <summary>
+    /// Extension methods for adding both MCWrapper.CLI and MCWrapper.RPC client libraries to an application's service container.
+    /// </summary>
+    public static class MultiChainCoreServices
+    {
+        /// <summary>
+        /// Add both MCWrapper.CLI and MCWrapper.RPC client libraries to an application service container.
+        /// <para>RpcOptions, CliOptions, and RuntimeParamOptions are parsed from the local environment variable store.</para>
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddMultiChainCoreServices(this IServiceCollection services)
+        {
+            services.AddMultiChainCoreCliServices();
+            services.AddMultiChainCoreRPCServices();
+
+            services.AddTransient<MCWrapperClientFactory>();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Add both MCWrapper.CLI and MCWrapper.RPC client libraries to an application service container.
+        /// <para>RpcOptions, CliOptions, and RuntimeParamOptions are parsed from the local IConfiguration pipeline.</para>
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddMultiChainCoreServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMultiChainCoreCliServices(configuration);
+            services.AddMultiChainCoreRPCServices(configuration);
+
+            services.AddTransient<MCWrapperClientFactory>();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Add both MCWrapper.CLI and MCWrapper.RPC client libraries to an application service container.
+        /// <para>RpcOptions, CliOptions, and RuntimeParamOptions are parsed from explicitily configured and passed Action parameters.</para>
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="rpcOptions"></param>
+        /// <param name="cliOptions"></param>
+        /// <param name="runtimeParamOptions"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddMultiChainCoreServices(this IServiceCollection services, Action<RpcOptions> rpcOptions, 
+            Action<CliOptions> cliOptions, [Optional] Action<RuntimeParamOptions> runtimeParamOptions)
+        {
+            services.AddMultiChainCoreCliServices(cliOptions, runtimeParamOptions);
+            services.AddMultiChainCoreRPCServices(rpcOptions, runtimeParamOptions);
+
+            services.AddTransient<MCWrapperClientFactory>();
+
+            return services;
+        }
+    }
+}
---
+++ MCWrapper/Factory/MCWrapperClientFactory.cs
@@ -0,0 +1,36 @@
+﻿using MCWrapper.CLI.Ledger.Factory;
+using MCWrapper.RPC.Ledger.Factory;
+
+namespace MCWrapper.Factory
+{
+    /// <summary>
+    /// MCWrapper client factory produces instances of the RpcClientFactory and the CliClientFactory
+    /// </summary>
+    public class MCWrapperClientFactory
+    {
+        /// <summary>
+        /// Create a new MCWrapperClientFactory instance
+        /// </summary>
+        /// <param name="rpcClientFactory"></param>
+        /// <param name="cliClientFactory"></param>
+        public MCWrapperClientFactory(RpcClientFactory rpcClientFactory, CliClientFactory cliClientFactory)
+        {
+            RpcClientFactory = rpcClientFactory;
+            CliClientFactory = cliClientFactory;
+        }
+
+        /// <summary>
+        /// GET: MCWrapper Remote Procedure Call (RPC) client factory
+        /// </summary>
+        /// <returns></returns>
+        public RpcClientFactory RpcClients => RpcClientFactory;
+        private readonly RpcClientFactory RpcClientFactory;
+
+        /// <summary>
+        /// GET: MCWrapper Command Line Interface (CLI) client factory
+        /// </summary>
+        /// <returns></returns>
+        public CliClientFactory CliClients => CliClientFactory;
+        private readonly CliClientFactory CliClientFactory;
+    }
+}
